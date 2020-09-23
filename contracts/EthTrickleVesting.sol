83eef3fb7b258469f526269eef4246cfac85d8a0
T. Thomas <TdotThomas@SpaceAgeMinds.com>
2020-09-22 19:01:55 -0700

    1. Added more code in the direction of a working contract.
    Notes. Currently still working and testing in remix.

--- contracts/EthTrickleVesting.sol
+++ contracts/EthTrickleVesting.sol
@@ -1,3 +1,5 @@
+pragma solidity >=0.4.22 <0.7.0;
+
 /**
  * @title EthTrickleVesting
  * @dev An eth holding contract that releases its eth balance gradually,
@@ -10,16 +12,27 @@ contract EthTrickleVesting {
 	* @param owner the address that can claim the eth and revoke the contract
 	*/
 	address private owner;
-
-	//duration
-	//Every duration the contract releases a trickle percentage of the vested eth amount. In days.
-	unit256 duration = 30;
-
-	//tricklePercentage
-	//The percentage to release or reinvest
-	uint256 tricklePercentage = 10;
-
-
+	
+	// event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    
+    /**
+     * @dev Change owner
+     * @param newOwner address of new owner
+     */
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+    
+    /**
+	* @dev Return owner address 
+	* @return address of owner
+	*/
+	function getOwner() external view returns (address) {
+		return owner;
+	}
+    
  	// modifier to check if caller is owner
 	modifier isOwner() {
 		// If the first argument of 'require' evaluates to 'false', execution terminates and all
@@ -30,6 +43,26 @@ contract EthTrickleVesting {
 		require(msg.sender == owner, "Caller is not owner");
 		_;
 	}
+
+	//duration
+	//Every duration the contract releases a trickle percentage of the vested eth amount. In days.
+	uint256 duration = 30;
+
+	//tricklePercentage
+	//The percentage to release or reinvest
+	uint256 tricklePercentage = 10;
+
+    //releasedAmount
+    //The amount of eth that can be releasedAmount
+    uint256 releasedAmount = 0;
+
+    //lockedAmount
+    //The amount of locked eth
+    uint256 lockedAmount = 0;
+
+    //totalAmount
+    //The total amount of eth in the contract
+    uint256 totalAmount = 0;
     
 	/**
 	* @dev Set contract deployer as owner
@@ -38,43 +71,45 @@ contract EthTrickleVesting {
 		owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
 		emit OwnerSet(address(0), owner);
 	}
-    
-	/**
-	* @dev Return owner address 
-	* @return address of owner
-	*/
-	function getOwner() external view returns (address) {
-		return owner;
-	}
 
 	//displayReleased
 	//Outputs the amount of eth avaliable for release.
-	function displayReleased() external view returns () {
+	function displayReleased() external view returns (uint256) {
+	    return releasedAmount;
 	}
 	
 	//displayLocked
 	//Outputs the amount of eth locked in the contract.
-	function displayLocked() external view returns () {
+	function displayLocked() external view returns (uint256) {
+	    return lockedAmount;
 	}
 
 	//displayTotal
 	//Outputs the total (Released+Locked) amount of eth in the contract.
-	function displayTotal() external view returns () {
+	function displayTotal() external view returns (uint256) {
+	    return totalAmount;
 	}	
 	
 	//claim function
 	//this function is used to claim all of the released eth.
-	function claim() {
-	}
+	function claim() public payable isOwner {
+        owner.send(msg.value);
+    }
+    
+    //lock function
+    //this fucntion is payable and locks the eth until its relased
+    function lock() public payable {
+        
+    }
 	
 	//release function
 	//After a specific duration a percentage of the eth vesting pool is move to the released amount
-	//If less then 1/100th of 1 eth then its all released.
-	function release() {
-	}
+	//If less then 1/100th of 1 eth then its all released, time based.
+	//function release() {
+	//}
 
 	//reinvest function
-	//After a specific duration a percentage of the released amount is moved back to the eth vesting pool.
-	function reinvest() {
-	}
+	//After a specific duration a percentage of the released amount is moved back to the eth vesting pool, time based.
+	//function reinvest() {
+	//}
 }
